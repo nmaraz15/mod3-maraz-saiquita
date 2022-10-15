69f62d2e49ff393d207bd2b56f0657da1cad8015
Natalia Maraz <nataliamaraz@gmail.com>
2022-10-14 23:51:07 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { LocationProvider } from './contexts/LocationContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
           <LocationProvider>
             <App />
           </LocationProvider>
         </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
