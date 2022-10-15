bbe82b91491c7c084488353645cccbcfd285eafa
Natalia Maraz <nataliamaraz@gmail.com>
2022-10-14 23:48:10 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; //
+import { HashRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import { LocationProvider } from './contexts/LocationContext';
 
